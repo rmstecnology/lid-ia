c4052e0375a1c7e3190fdbf422309be697255cf5
rmstecnology <154079383+rmstecnology@users.noreply.github.com>
2023-12-17 19:46:29 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// script.js
+
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+let totalSegundos = 600; // 10 minutos em segundos
+
+function atualizarRelogio() {
+    const mins = Math.floor(totalSegundos / 60);
+    const segs = totalSegundos % 60;
+
+    minutos.textContent = String(mins).padStart(2, '0');
+    segundos.textContent = String(segs).padStart(2, '0');
+}
+
+const relogio = setInterval(function() {
+    if (totalSegundos > 0) {
+        totalSegundos--;
+        atualizarRelogio();
+    } else {
+        clearInterval(relogio);
+        alert("Cronômetro chegou a zero!");
+    }
+}, 1000);
+
+// Inicializar o relógio ao carregar a página
+atualizarRelogio();
